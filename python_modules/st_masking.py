64abdd2656926e15183dc3176925bfbf0b1c3658
AntoineGuenette <anto.guenette@gmail.com>
2025-07-02 16:28:10 -0400

    Add template for first ST python module

---
+++ python_modules/st_masking.py
@@ -0,0 +1,217 @@
+import ismrmrd
+import os
+import logging
+import traceback
+import numpy as np
+import matplotlib.pyplot as plt
+import xml.dom.minidom
+import base64
+import mrdhelper
+import constants
+
+# Folder for debug output files
+debugFolder = "/tmp/share/debug"
+
+def process(connection, config, mrdHeader):
+    logging.info("Config: \n%s", config)
+
+    # mrdHeader should be xml formatted MRD header, but may be a string
+    # if it failed conversion earlier
+    try:
+        # Disabled due to incompatibility between PyXB and Python 3.8:
+        # https://github.com/pabigot/pyxb/issues/123
+        # # logging.info("MRD header: \n%s", mrdHeader.toxml('utf-8'))
+
+        logging.info("Incoming dataset contains %d encodings", len(mrdHeader.encoding))
+        logging.info("First encoding is of type '%s', with a matrix size of (%s x %s x %s) and a field of view of (%s x %s x %s)mm^3", 
+            mrdHeader.encoding[0].trajectory, 
+            mrdHeader.encoding[0].encodedSpace.matrixSize.x, 
+            mrdHeader.encoding[0].encodedSpace.matrixSize.y, 
+            mrdHeader.encoding[0].encodedSpace.matrixSize.z, 
+            mrdHeader.encoding[0].encodedSpace.fieldOfView_mm.x, 
+            mrdHeader.encoding[0].encodedSpace.fieldOfView_mm.y, 
+            mrdHeader.encoding[0].encodedSpace.fieldOfView_mm.z)
+
+    except:
+        logging.info("Improperly formatted MRD header: \n%s", mrdHeader)
+
+    # Continuously parse incoming data parsed from MRD messages
+    currentSeries = 0
+    imgGroup = []
+    waveformGroup = []
+    try:
+        for item in connection:
+            # ----------------------------------------------------------
+            # Image data messages
+            # ----------------------------------------------------------
+            if isinstance(item, ismrmrd.Image):
+                # When this criteria is met, run process_group() on the accumulated
+                # data, which returns images that are sent back to the client.
+                # e.g. when the series number changes:
+                if item.image_series_index != currentSeries:
+                    logging.info("Processing a group of images because series index changed to %d", item.image_series_index)
+                    currentSeries = item.image_series_index
+                    image = process_image(imgGroup, connection, config, mrdHeader)
+                    connection.send_image(image)
+                    imgGroup = []
+
+            # ----------------------------------------------------------
+            # Waveform data messages
+            # ----------------------------------------------------------
+            elif isinstance(item, ismrmrd.Waveform):
+                waveformGroup.append(item)
+
+            elif item is None:
+                break
+
+            else:
+                logging.error("Unsupported data type %s", type(item).__name__)
+
+        # Extract raw ECG waveform data. Basic sorting to make sure that data 
+        # is time-ordered, but no additional checking for missing data.
+        # ecgData has shape (5 x timepoints)
+        if len(waveformGroup) > 0:
+            waveformGroup.sort(key = lambda item: item.time_stamp)
+            ecgData = [item.data for item in waveformGroup if item.waveform_id == 0]
+            if len(ecgData) > 0:
+                ecgData = np.concatenate(ecgData,1)
+
+        # Process any remaining groups of image data.  This can 
+        # happen if the trigger condition for these groups are not met.
+        # This is also a fallback for handling image data, as the last
+        # image in a series is typically not separately flagged.
+        if len(imgGroup) > 0:
+            logging.info("Processing a group of images (untriggered)")
+            image = process_image(imgGroup, connection, config, mrdHeader)
+            connection.send_image(image)
+            imgGroup = []
+
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        connection.send_logging(constants.MRD_LOGGING_ERROR, traceback.format_exc())
+
+    finally:
+        connection.send_close()
+
+
+def process_image(imgGroup, connection, config, mrdHeader):
+    if len(imgGroup) == 0:
+        return []
+
+    logging.info('-----------------------------------------------')
+    logging.info(f'process_image called with {len(imgGroup)} images')
+    logging.info('-----------------------------------------------')
+
+    # Create folder, if necessary
+    if not os.path.exists(debugFolder):
+        os.makedirs(debugFolder)
+        logging.debug("Created folder " + debugFolder + " for debug output files")
+
+    logging.debug("Processing data with %d images of type %s", len(imgGroup), ismrmrd.get_dtype_from_data_type(imgGroup[0].data_type))
+
+    # Note: The MRD Image class stores data as [cha z y x]
+
+    # Extract image data into a 5D array of size [img cha z y x]
+    data = np.stack([img.data                              for img in imgGroup])
+    head = [img.getHead()                                  for img in imgGroup]
+    meta = [ismrmrd.Meta.deserialize(img.attribute_string) for img in imgGroup]
+
+    # Reformat data to [y x z cha img], i.e. [row col] for the first two dimensions
+    data = data.transpose((3, 4, 2, 1, 0))
+
+    # Display MetaAttributes for first image
+    logging.debug("MetaAttributes[0]: %s", ismrmrd.Meta.serialize(meta[0]))
+
+    # Optional serialization of ICE MiniHeader
+    if 'IceMiniHead' in meta[0]:
+        logging.debug("IceMiniHead[0]: %s", base64.b64decode(meta[0]['IceMiniHead']).decode('utf-8'))
+
+    logging.debug("Original image data is size %s" % (data.shape,))
+    np.save(debugFolder + "/" + "imgOrig.npy", data)
+
+    #TODO: Convert MRD image to NIFTI image
+
+    if mrdhelper.get_json_config_param(config, 'method') == 'threshold':
+        pass #TODO: Implement thresholding
+
+    if mrdhelper.get_json_config_param(config, 'method') == 'sct_deepseg':
+        pass #TODO: Implement SCT_deepseg
+
+    if mrdhelper.get_json_config_param(config, 'method') == 'bet':
+        pass #TODO: Implement BET
+
+    currentSeries = 0
+
+    # Re-slice back into 2D images
+    imagesOut = [None] * data.shape[-1]
+    for iImg in range(data.shape[-1]):
+        # Create new MRD instance for the inverted image
+        # Transpose from convenience shape of [y x z cha] to MRD Image shape of [cha z y x]
+        # from_array() should be called with 'transpose=False' to avoid warnings, and when called
+        # with this option, can take input as: [cha z y x], [z y x], or [y x]
+        imagesOut[iImg] = ismrmrd.Image.from_array(data[...,iImg].transpose((3, 2, 0, 1)), transpose=False)
+
+        # Create a copy of the original fixed header and update the data_type
+        # (we changed it to int16 from all other types)
+        oldHeader = head[iImg]
+        oldHeader.data_type = imagesOut[iImg].data_type
+
+        # Set the image_type to match the data_type for complex data
+        if (imagesOut[iImg].data_type == ismrmrd.DATATYPE_CXFLOAT) or (imagesOut[iImg].data_type == ismrmrd.DATATYPE_CXDOUBLE):
+            oldHeader.image_type = ismrmrd.IMTYPE_COMPLEX
+
+        if mrdhelper.get_json_config_param(config, 'options') == 'rgb':
+            # Set RGB parameters
+            oldHeader.image_type = 6  # To be defined as ismrmrd.IMTYPE_RGB
+            oldHeader.channels   = 3  # RGB "channels".  This is set by from_array, but need to be explicit as we're copying the old header instead
+
+        # Unused example, as images are grouped by series before being passed into this function now
+        # oldHeader.image_series_index = currentSeries
+
+        # Increment series number when flag detected (i.e. follow ICE logic for splitting series)
+        if mrdhelper.get_meta_value(meta[iImg], 'IceMiniHead') is not None:
+            if mrdhelper.extract_minihead_bool_param(base64.b64decode(meta[iImg]['IceMiniHead']).decode('utf-8'), 'BIsSeriesEnd') is True:
+                currentSeries += 1
+
+        imagesOut[iImg].setHead(oldHeader)
+
+        # Create a copy of the original ISMRMRD Meta attributes and update
+        tmpMeta = meta[iImg]
+        tmpMeta['DataRole']                       = 'Image'
+        tmpMeta['ImageProcessingHistory']         =  None # TODO: Add processing history if needed
+        tmpMeta['SequenceDescriptionAdditional']  = 'FIRE'
+        tmpMeta['Keep_image_geometry']            = 1
+        # TODO : Verify if these attributes are needed and add other ones if necessary
+
+        # Add image orientation directions to MetaAttributes if not already present
+        if tmpMeta.get('ImageRowDir') is None:
+            tmpMeta['ImageRowDir'] = ["{:.18f}".format(oldHeader.read_dir[0]), "{:.18f}".format(oldHeader.read_dir[1]), "{:.18f}".format(oldHeader.read_dir[2])]
+
+        if tmpMeta.get('ImageColumnDir') is None:
+            tmpMeta['ImageColumnDir'] = ["{:.18f}".format(oldHeader.phase_dir[0]), "{:.18f}".format(oldHeader.phase_dir[1]), "{:.18f}".format(oldHeader.phase_dir[2])]
+
+        metaXml = tmpMeta.serialize()
+        logging.debug("Image MetaAttributes: %s", xml.dom.minidom.parseString(metaXml).toprettyxml())
+        logging.debug("Image data has %d elements", imagesOut[iImg].data.size)
+
+        imagesOut[iImg].attribute_string = metaXml
+
+    # Send a copy of original (unmodified) images back too
+    if mrdhelper.get_json_config_param(config, 'sendOriginal', default=False, type='bool') == True:
+        logging.info('Sending a copy of original unmodified images due to sendOriginal set to True')
+        # In reverse order so that they'll be in correct order as we insert them to the front of the list
+        for image in reversed(imgGroup):
+            # Create a copy to not modify the original inputs
+            tmpImg = image
+
+            # Change the series_index to have a different series
+            tmpImg.image_series_index = 99
+
+            # Ensure Keep_image_geometry is set to not reverse image orientation
+            tmpMeta = ismrmrd.Meta.deserialize(tmpImg.attribute_string)
+            tmpMeta['Keep_image_geometry'] = 1
+            tmpImg.attribute_string = tmpMeta.serialize()
+
+            imagesOut.insert(0, tmpImg)
+
+    return imagesOut
