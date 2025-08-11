99d84bbe192d6c3746d6023a5e6ea48bce19c4e1
AntoineGuenette <anto.guenette@gmail.com>
2025-08-11 12:11:26 -0400

    Add back the necessary process_raw function

--- python_modules/st_masking.py
+++ python_modules/st_masking.py
@@ -3,12 +3,16 @@ import os
 import logging
 import traceback
 import numpy as np
+import numpy.fft as fft
 import xml.dom.minidom
 import base64
-import mrdhelper
-import constants
+import ctypes
+import mrdhelper # Custom module for MRD helper functions found in the python-ismrmrd-server repository
+import constants # Custom module for constants found in the python-ismrmrd-server repository
 import subprocess
 import nibabel as nib
+import shutil
+from time import perf_counter
 
 # Folder for debug output files
 debugFolder = "/tmp/share/debug"
@@ -38,14 +42,34 @@ def process(connection, config, mrdHeader):
 
     # Continuously parse incoming data parsed from MRD messages
     currentSeries = 0
+    acqGroup = []
     imgGroup = []
     waveformGroup = []
     try:
         for item in connection:
             # ----------------------------------------------------------
+            # Raw k-space data messages
+            # ----------------------------------------------------------
+            if isinstance(item, ismrmrd.Acquisition):
+                # Accumulate all imaging readouts in a group
+                if (not item.is_flag_set(ismrmrd.ACQ_IS_NOISE_MEASUREMENT) and
+                    not item.is_flag_set(ismrmrd.ACQ_IS_PARALLEL_CALIBRATION) and
+                    not item.is_flag_set(ismrmrd.ACQ_IS_PHASECORR_DATA) and
+                    not item.is_flag_set(ismrmrd.ACQ_IS_NAVIGATION_DATA)):
+                    acqGroup.append(item)
+
+                # When this criteria is met, run process_raw() on the accumulated
+                # data, which returns images that are sent back to the client.
+                if item.is_flag_set(ismrmrd.ACQ_LAST_IN_SLICE):
+                    logging.info("Processing a group of k-space data")
+                    image = process_raw(acqGroup, connection, config, mrdHeader)
+                    connection.send_image(image)
+                    acqGroup = []
+
+            # ----------------------------------------------------------
             # Image data messages
             # ----------------------------------------------------------
-            if isinstance(item, ismrmrd.Image):
+            elif isinstance(item, ismrmrd.Image):
                 # When this criteria is met, run process_group() on the accumulated
                 # data, which returns images that are sent back to the client.
                 # e.g. when the series number changes:
@@ -55,6 +79,17 @@ def process(connection, config, mrdHeader):
                     image = process_image(imgGroup, connection, config, mrdHeader)
                     connection.send_image(image)
                     imgGroup = []
+
+                # Only process magnitude images -- send phase images back without modification (fallback for images with unknown type)
+                if (item.image_type is ismrmrd.IMTYPE_MAGNITUDE) or (item.image_type == 0):
+                    imgGroup.append(item)
+                else:
+                    tmpMeta = ismrmrd.Meta.deserialize(item.attribute_string)
+                    tmpMeta['Keep_image_geometry']    = 1
+                    item.attribute_string = tmpMeta.serialize()
+
+                    connection.send_image(item)
+                    continue
 
             # ----------------------------------------------------------
             # Waveform data messages
@@ -77,10 +112,16 @@ def process(connection, config, mrdHeader):
             if len(ecgData) > 0:
                 ecgData = np.concatenate(ecgData,1)
 
-        # Process any remaining groups of image data.  This can 
+        # Process any remaining groups of raw or image data.  This can 
         # happen if the trigger condition for these groups are not met.
         # This is also a fallback for handling image data, as the last
         # image in a series is typically not separately flagged.
+        if len(acqGroup) > 0:
+            logging.info("Processing a group of k-space data (untriggered)")
+            image = process_raw(acqGroup, connection, config, mrdHeader)
+            connection.send_image(image)
+            acqGroup = []
+
         if len(imgGroup) > 0:
             logging.info("Processing a group of images (untriggered)")
             image = process_image(imgGroup, connection, config, mrdHeader)
@@ -93,6 +134,120 @@ def process(connection, config, mrdHeader):
 
     finally:
         connection.send_close()
+
+
+def process_raw(acqGroup, connection, config, mrdHeader):
+    if len(acqGroup) == 0:
+        return []
+    
+    logging.info(f'-----------------------------------------------')
+    logging.info(f'     process_raw called with {len(acqGroup)} readouts')
+    logging.info(f'-----------------------------------------------')
+
+    # Start timer
+    tic = perf_counter()
+
+    # Create folder, if necessary
+    if not os.path.exists(debugFolder):
+        os.makedirs(debugFolder)
+        logging.debug("Created folder " + debugFolder + " for debug output files")
+
+    # Format data into single [cha PE RO phs] array
+    lin = [acquisition.idx.kspace_encode_step_1 for acquisition in acqGroup]
+    phs = [acquisition.idx.phase                for acquisition in acqGroup]
+
+    # Use the zero-padded matrix size
+    data = np.zeros((acqGroup[0].data.shape[0], 
+                     mrdHeader.encoding[0].encodedSpace.matrixSize.y, 
+                     mrdHeader.encoding[0].encodedSpace.matrixSize.x, 
+                     max(phs)+1), 
+                    acqGroup[0].data.dtype)
+
+    rawHead = [None]*(max(phs)+1)
+
+    for acq, lin, phs in zip(acqGroup, lin, phs):
+        if (lin < data.shape[1]) and (phs < data.shape[3]):
+            # TODO: Account for asymmetric echo in a better way
+            data[:,lin,-acq.data.shape[1]:,phs] = acq.data
+
+            # center line of k-space is encoded in user[5]
+            if (rawHead[phs] is None) or (np.abs(acq.getHead().idx.kspace_encode_step_1 - acq.getHead().idx.user[5]) < np.abs(rawHead[phs].idx.kspace_encode_step_1 - rawHead[phs].idx.user[5])):
+                rawHead[phs] = acq.getHead()
+
+    # Flip matrix in RO/PE to be consistent with ICE
+    data = np.flip(data, (1, 2))
+
+    logging.debug("Raw data is size %s" % (data.shape,))
+    np.save(debugFolder + "/" + "raw.npy", data)
+
+    # Fourier Transform
+    data = fft.fftshift( data, axes=(1, 2))
+    data = fft.ifft2(    data, axes=(1, 2))
+    data = fft.ifftshift(data, axes=(1, 2))
+    data *= np.prod(data.shape) # FFT scaling for consistency with ICE
+
+    # Sum of squares coil combination
+    # Data will be [PE RO phs]
+    data = np.abs(data)
+    data = np.square(data)
+    data = np.sum(data, axis=0)
+    data = np.sqrt(data)
+
+    logging.debug("Image data is size %s" % (data.shape,))
+    np.save(debugFolder + "/" + "img.npy", data)
+
+    # Remove readout oversampling
+    if mrdHeader.encoding[0].reconSpace.matrixSize.x != 0:
+        offset = int((data.shape[1] - mrdHeader.encoding[0].reconSpace.matrixSize.x)/2)
+        data = data[:,offset:offset+mrdHeader.encoding[0].reconSpace.matrixSize.x]
+
+    # Remove phase oversampling
+    if mrdHeader.encoding[0].reconSpace.matrixSize.y != 0:
+        offset = int((data.shape[0] - mrdHeader.encoding[0].reconSpace.matrixSize.y)/2)
+        data = data[offset:offset+mrdHeader.encoding[0].reconSpace.matrixSize.y,:]
+
+    logging.debug("Image without oversampling is size %s" % (data.shape,))
+    np.save(debugFolder + "/" + "imgCrop.npy", data)
+
+    # Measure processing time
+    toc = perf_counter()
+    strProcessTime = "Total processing time: %.2f ms" % ((toc-tic)*1000.0)
+    logging.info(strProcessTime)
+
+    # Send this as a text message back to the client
+    connection.send_logging(constants.MRD_LOGGING_INFO, strProcessTime)
+
+    # Format as ISMRMRD image data
+    imagesOut = []
+    for phs in range(data.shape[2]):
+        # Create new MRD instance for the processed image
+        # data has shape [PE RO phs], i.e. [y x].
+        # from_array() should be called with 'transpose=False' to avoid warnings, and when called
+        # with this option, can take input as: [cha z y x], [z y x], or [y x]
+        tmpImg = ismrmrd.Image.from_array(data[...,phs], transpose=False)
+
+        # Set the header information
+        tmpImg.setHead(mrdhelper.update_img_header_from_raw(tmpImg.getHead(), rawHead[phs]))
+        tmpImg.field_of_view = (ctypes.c_float(mrdHeader.encoding[0].reconSpace.fieldOfView_mm.x), 
+                                ctypes.c_float(mrdHeader.encoding[0].reconSpace.fieldOfView_mm.y), 
+                                ctypes.c_float(mrdHeader.encoding[0].reconSpace.fieldOfView_mm.z))
+        tmpImg.image_index = phs
+
+        # Set ISMRMRD Meta Attributes
+        tmpMeta = ismrmrd.Meta()
+        tmpMeta['DataRole']               = 'Image'
+        tmpMeta['ImageProcessingHistory'] = ['FIRE', 'PYTHON']
+        tmpMeta['Keep_image_geometry']    = 1
+
+        xml = tmpMeta.serialize()
+        logging.debug("Image MetaAttributes: %s", xml)
+        tmpImg.attribute_string = xml
+        imagesOut.append(tmpImg)
+
+    # Call process_image() to invert image contrast
+    imagesOut = process_image(imagesOut, connection, config, mrdHeader)
+
+    return imagesOut
 
 
 def process_image(imgGroup, connection, config, mrdHeader):
@@ -116,12 +271,40 @@ def process_image(imgGroup, connection, config, mrdHeader):
     data = np.stack([img.data                              for img in imgGroup])
     head = [img.getHead()                                  for img in imgGroup]
     meta = [ismrmrd.Meta.deserialize(img.attribute_string) for img in imgGroup]
-    # TODO : Verify if necessary or adapt for new MRD version
+    
+    # Reformat data to [y x z cha img], i.e. [row col] for the first two dimensions
+    data = data.transpose((3, 4, 2, 1, 0))
+    
+     # Display MetaAttributes for first image
+    logging.debug("MetaAttributes[0]: %s", ismrmrd.Meta.serialize(meta[0]))
+
+    # Optional serialization of ICE MiniHeader
+    if 'IceMiniHead' in meta[0]:
+        logging.debug("IceMiniHead[0]: %s", base64.b64decode(meta[0]['IceMiniHead']).decode('utf-8'))
+
+    logging.debug("Original image data is size %s" % (data.shape,))
+    np.save(debugFolder + "/imgOrig.npy", data)
+
+    # Copy the MRD file to a new location for conversion
+    mrd2nii_folder = debugFolder + "/mrd2nii_conversion"
+    if not os.path.exists(mrd2nii_folder):
+        os.makedirs(mrd2nii_folder)
+    fname_input_mrd = connection.mrdFilePath # Currently returns None. TODO: Should be changed the path to the .h5 file
+    fname_copied_mrd = os.path.join(mrd2nii_folder, "imgOrig.h5")
+    shutil.copy(fname_input_mrd, fname_copied_mrd)
 
     # Convert the MRD images to NIfTI format
-    fname_input_mrd = None #TODO to identify
-    fname_input_nii = debugFolder + '/imgOrig.nii.gz'
-    subprocess.run(['mrd2nii', fname_input_mrd, fname_input_nii], check=True)
+    subprocess.run(['mrd2nii', '-i', mrd2nii_folder, '-o', mrd2nii_folder], check=True)
+
+    # Rename the output NIfTI file
+    fname_input_nii = os.path.join(mrd2nii_folder, "imgOrig.nii.gz")
+    nii_files = [f for f in os.listdir(mrd2nii_folder) if f.endswith('.nii.gz')]
+    if not nii_files:
+        raise FileNotFoundError("No .nii.gz file found in {}".format(mrd2nii_folder))
+    first_nii = os.path.join(mrd2nii_folder, nii_files[0])
+    os.rename(first_nii, fname_input_nii)
+
+    fname_output_mask_nii = debugFolder + '/mask_threshold.nii.gz'
 
     # Create the mask with the threshold method
     if mrdhelper.get_json_config_param(config, 'method') == 'threshold':
@@ -131,18 +314,20 @@ def process_image(imgGroup, connection, config, mrdHeader):
                         '--thr', mrdhelper.get_json_config_param(config, 'value'),
                         '-o', fname_output_mask_nii],
                         check=True)
+        output_mask_nii = nib.load(fname_output_mask_nii)
+        data = output_mask_nii.get_fdata()
 
     # Create the mask with the SC segmentation method
-    if mrdhelper.get_json_config_param(config, 'method') == 'sct_deepseg':
-        pass #TODO: Implement SCT_deepseg
-        # Install the model if not already installed
+    elif mrdhelper.get_json_config_param(config, 'method') == 'sct_deepseg':
+        raise NotImplementedError(f"Method {mrdhelper.get_json_config_param(config, 'method')} is not implemented yet")
 
     # Create the mask with the brain segmentation method
-    if mrdhelper.get_json_config_param(config, 'method') == 'bet':
-        pass #TODO: Implement BET
+    elif mrdhelper.get_json_config_param(config, 'method') == 'bet':
+        raise NotImplementedError(f"Method {mrdhelper.get_json_config_param(config, 'method')} is not implemented yet")
 
-    output_mask_nii = nib.load(fname_output_mask_nii)
-    data = output_mask_nii.get_fdata()
+    else :
+        raise RuntimeError(f"Method {mrdhelper.get_json_config_param(config, 'method')} is not available. Options are : \
+                           'threshold', 'sct_deepseg' and 'bet'.")
 
     currentSeries = 0
 
@@ -161,10 +346,6 @@ def process_image(imgGroup, connection, config, mrdHeader):
         oldHeader = head[iImg]
         oldHeader.data_type = imagesOut[iImg].data_type
 
-        # Set the image_type to match the data_type for complex data
-        if (imagesOut[iImg].data_type == ismrmrd.DATATYPE_CXFLOAT) or (imagesOut[iImg].data_type == ismrmrd.DATATYPE_CXDOUBLE):
-            oldHeader.image_type = ismrmrd.IMTYPE_COMPLEX
-
         # Increment series number when flag detected (i.e. follow ICE logic for splitting series)
         if mrdhelper.get_meta_value(meta[iImg], 'IceMiniHead') is not None:
             if mrdhelper.extract_minihead_bool_param(base64.b64decode(meta[iImg]['IceMiniHead']).decode('utf-8'), 'BIsSeriesEnd') is True:
@@ -175,10 +356,10 @@ def process_image(imgGroup, connection, config, mrdHeader):
         # Create a copy of the original ISMRMRD Meta attributes and update
         tmpMeta = meta[iImg]
         tmpMeta['DataRole']                       = 'Image'
-        tmpMeta['ImageProcessingHistory']         =  None # TODO: Add processing history if needed
+        tmpMeta['ImageProcessingHistory']         = ['PYTHON', 'THRESHOLD'] #TODO : May need to be updated
         tmpMeta['SequenceDescriptionAdditional']  = 'FIRE'
         tmpMeta['Keep_image_geometry']            = 1
-        # TODO : Verify if these attributes are needed and add other ones if necessary
+        # TODO : Add other attributes for other methohds yet to be implemented if necessary
 
         # Add image orientation directions to MetaAttributes if not already present
         if tmpMeta.get('ImageRowDir') is None:
