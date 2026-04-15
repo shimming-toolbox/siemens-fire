a1d8ae97b61605206901310ae45e5d304a728cc4
ANREI31 <anrei31@ulaval.ca>
2026-04-15 14:53:16 -0400

    Update README; Change port in launch.json; add new module

---
+++ python_modules/navi_corr.py
@@ -0,0 +1,77 @@
+import ismrmrd
+import os
+import itertools
+import logging
+import traceback
+import numpy as np
+import numpy.fft as fft
+import matplotlib.pyplot as plt
+import xml.dom.minidom
+import base64
+import ctypes
+import re
+import mrdhelper
+import constants
+from time import perf_counter
+
+# Folder for debug output files
+debugFolder = "/tmp/share/debug"
+
+def process(connection, config, mrdHeader):
+    logging.info("Config: \n%s", config)
+
+    # mrdHeader should be xml formatted MRD header, but may be a string
+    # if it failed conversion earlier
+    try:
+        # Disabled due to incompatibility between PyXB and Python 3.8:
+        # https://github.com/pabigot/pyxb/issues/123
+        # # logging.info("MRD header: \n%s", mrdHeader.toxml('utf-8'))
+
+        logging.info("Incoming dataset contains %d encodings", len(mrdHeader.encoding))
+        logging.info("First encoding is of type '%s', with a matrix size of (%s x %s x %s) and a field of view of (%s x %s x %s)mm^3", 
+            mrdHeader.encoding[0].trajectory, 
+            mrdHeader.encoding[0].encodedSpace.matrixSize.x, 
+            mrdHeader.encoding[0].encodedSpace.matrixSize.y, 
+            mrdHeader.encoding[0].encodedSpace.matrixSize.z, 
+            mrdHeader.encoding[0].encodedSpace.fieldOfView_mm.x, 
+            mrdHeader.encoding[0].encodedSpace.fieldOfView_mm.y, 
+            mrdHeader.encoding[0].encodedSpace.fieldOfView_mm.z)
+
+    except:
+        logging.info("Improperly formatted MRD header: \n%s", mrdHeader)
+
+    # Continuously parse incoming data parsed from MRD messages
+    currentSeries = 0
+    acqGroup = []
+    imgGroup = []
+    waveformGroup = []
+    try:
+        for item in connection:
+            # ----------------------------------------------------------
+            # Raw k-space data messages
+            # ----------------------------------------------------------
+            if isinstance(item, ismrmrd.Acquisition):
+                acqGroup.append(item)
+
+            elif item is None:
+                break
+
+            else:
+                logging.error("Unsupported data type %s", type(item).__name__)
+
+        # Process any remaining groups of raw or image data.  This can 
+        # happen if the trigger condition for these groups are not met.
+        # This is also a fallback for handling image data, as the last
+        # image in a series is typically not separately flagged.
+        if len(acqGroup) > 0:
+            logging.info("Processing a group of k-space data (untriggered)")
+            #image = process_raw(acqGroup, connection, config, mrdHeader)
+            #connection.send_image(image)
+            acqGroup = []
+
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        connection.send_logging(constants.MRD_LOGGING_ERROR, traceback.format_exc())
+
+    finally:
+        connection.send_close()
