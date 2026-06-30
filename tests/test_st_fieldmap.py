b8b09d9657af3f6a749ee637c8de744abeaefe55
Alex Dastous <po09i@hotmail.com>
2026-06-30 15:52:43 -0400

    Update log file name

--- tests/test_st_fieldmap.py
+++ tests/test_st_fieldmap.py
@@ -18,7 +18,7 @@ def test_st_fieldmap_use_mask(tmpdir):
     fname_mask = os.path.join(__PATH_TESTING_DATA__, "gre_mask.nii.gz")
     shutil.copy(fname_mask, os.path.join(__TMP_SHARE_SAVEDDATA__, "mask.nii.gz"))
 
-    fname_logfile = os.path.join(__PATH_TESTING_DATA__, "client.log")
+    fname_logfile = os.path.join(__PATH_TESTING_DATA__, "test_st_fieldmap_use_mask.log")
     if os.path.exists(fname_logfile):
         os.remove(fname_logfile)
 
@@ -98,7 +98,7 @@ def test_st_fieldmap_use_mask_dilate(tmpdir):
     fname_mask = os.path.join(__PATH_TESTING_DATA__, "gre_mask.nii.gz")
     shutil.copy(fname_mask, os.path.join(__TMP_SHARE_SAVEDDATA__, "mask.nii.gz"))
 
-    fname_logfile = os.path.join(__PATH_TESTING_DATA__, "client.log")
+    fname_logfile = os.path.join(__PATH_TESTING_DATA__, "test_st_fieldmap_use_mask_dilate.log")
     if os.path.exists(fname_logfile):
         os.remove(fname_logfile)
 
